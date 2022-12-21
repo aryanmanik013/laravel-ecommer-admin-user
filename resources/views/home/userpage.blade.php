174bfc63a180f8e5a5861e291a3f3e73837b86d3
aryanmanik013 <aryanmanik440@gmail.com>
2022-12-21 14:19:51 +0530

    first-commit

---
+++ resources/views/home/userpage.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+@include('home.includes');
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        @include('home.header');
+        <!-- end header section -->
+        <!-- slider section -->
+        @include('home.slider');
+        <!-- end slider section -->
+    </div>
+    <!-- why section -->
+    @include('home.why');
+
+    <!-- end why section -->
+
+    <!-- arrival section -->
+    @include('home.arrival');
+
+    <!-- end arrival section -->
+
+    <!-- product section -->
+    @include('home.product');
+
+    <!-- end product section -->
+
+    <!-- subscribe section -->
+    @include('home.subscribe');
+
+    <!-- end subscribe section -->
+    <!-- client section -->
+    @include('home.clients');
+
+    <!-- end client section -->
+    <!-- footer start -->
+
+    @include('home.footer');
+
+    <!-- footer end -->
+    <div class="cpy_">
+        <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+
+            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+
+        </p>
+    </div>
+    <!-- jQery -->
+    <script src="home/js/jquery-3.4.1.min.js"></script>
+    <!-- popper js -->
+    <script src="home/js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="home/js/bootstrap.js"></script>
+    <!-- custom js -->
+    <script src="home/js/custom.js"></script>
+</body>
+
+</html>
